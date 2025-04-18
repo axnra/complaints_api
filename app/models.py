ac8ee39e792ae8c3e0742035e137652edca3501f
Evgeniy <axnra@bk.ru>
2025-04-18 20:10:20 +0800

    fix timestamps autoincrement

--- app/models.py
+++ app/models.py
@@ -23,6 +23,6 @@ class Complaint(Base):
     id = Column(Integer, primary_key=True, index=True)
     text = Column(String, nullable=False)
     status = Column(String, default="open")
-    timestamp = Column(DateTime, default=datetime.now(timezone.utc))
+    timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     sentiment = Column(String, default="unknown")
     category = Column(String, default="другое")
